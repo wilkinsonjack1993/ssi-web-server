f9ef0e80206136fcc1c1fe2c5e9e7815d59ef43b
Jack Wilkinson <jack.wilkinson@mdpi.com>
2022-03-24 22:46:29 +0100

    SSI now works recursively

---
+++ htmlParser.js
@@ -0,0 +1,110 @@
+const fs = require("fs").promises;
+
+const ATTRIBUTE_MATCHER = /([a-z]+)="(.+?)"/g;
+
+// This method was taken from: https://dev.to/ycmjason/stringprototypereplace-asynchronously-28k9
+const asyncStringReplace = async (str, regex, aReplacer) => {
+  const substrs = [];
+  let match;
+  let i = 0;
+  while ((match = regex.exec(str)) !== null) {
+    // put non matching string
+    substrs.push(str.slice(i, match.index));
+    // call the async replacer function with the matched array spreaded
+    substrs.push(aReplacer(...match));
+    i = regex.lastIndex;
+  }
+  // put the rest of str
+  substrs.push(str.slice(i));
+  // wait for aReplacer calls to finish and join them back into string
+  return (await Promise.all(substrs)).join("");
+};
+
+class SSIHtmlParser {
+  constructor(filepath) {
+    this.filepath = filepath;
+    this.file = null;
+  }
+
+  // This method was taken from: https://github.com/kidwm/node-ssi/blob/166c716cbc22ecaf340d63093977785942a988ef/lib/DirectiveHandler.js#L113
+  parseAttributes(directive) {
+    const attributes = [];
+
+    directive.replace(ATTRIBUTE_MATCHER, function (attribute, name, value) {
+      console.log(attribute, name, value);
+      attributes.push({ name: name, value: value });
+    });
+    console.log(attributes);
+    return attributes;
+  }
+
+  async directiveParser(directive, directiveName) {
+    if (directiveName !== "include") {
+      console.error(
+        "Unknown directive: " +
+          directiveName +
+          '. Currently only "include" is supported.'
+      );
+
+      // Could replace this with an error widget - for now just do load nothing.
+      return "";
+    }
+    console.log(directive);
+
+    const attributes = this.parseAttributes(directive);
+
+    // Check that attribute is of correct type
+    if (attributes.length !== 1 || attributes[0].name !== "file") {
+      console.error(
+        "Include directive currently only supports one file attribute"
+      );
+    }
+
+    const file = attributes[0].value;
+    console.log("file: ", file);
+    const newFilepath = this.filepath.replace(
+      /([^\/]+$)/,
+      file[0] === "/" ? file.substring(1) : file
+    );
+    console.log("new file passer", newFilepath);
+
+    const newFileParser = new SSIHtmlParser(newFilepath);
+    const parsedResult = await newFileParser.parse();
+    return parsedResult.toString();
+  }
+
+  async parseServerSideIncludes() {
+    const htmlString = this.file.toString();
+
+    const parsedHtmlString = await asyncStringReplace(
+      htmlString,
+      /<!--#([a-z]+)([ ]+([a-z]+)="(.+?)")* -->/g,
+      this.directiveParser.bind(this)
+    );
+
+    console.log(parsedHtmlString);
+
+    // TODO - turn back to buffer?
+    return parsedHtmlString;
+  }
+
+  parse() {
+    const isSSI = this.filepath.endsWith(".shtml");
+
+    return fs
+      .readFile(this.filepath)
+      .then((file) => {
+        // If file is not an SSI file, return the file as is.
+        if (!isSSI) return file;
+
+        // else parse the file
+        this.file = file;
+        return this.parseServerSideIncludes();
+      })
+      .catch((err) => {
+        throw err;
+      });
+  }
+}
+
+module.exports = { SSIHtmlParser };
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require("http");
-const fs = require("fs").promises;
 const { createReadStream } = require("fs");
+const { SSIHtmlParser } = require("./htmlParser");
 
 const DEFAULT_HOST = "localhost";
 const DEFAULT_PORT = 8000;
@@ -9,38 +9,39 @@ const DEFAULT_PORT = 8000;
 
 const serveAdobeFavicon = function (req, res) {
   res.setHeader("Content-Type", "image/x-icon");
-  createReadStream(__dirname + `/app${req.url}`)
-    .pipe(res)
-    .catch((err) => {
-      res.writeHead(500);
-      res.end(err);
-      return;
-    });
+  try {
+    createReadStream(__dirname + `/app${req.url}`).pipe(res);
+  } catch (err) {
+    res.writeHead(500);
+    res.end(err);
+  }
 };
 
 // This is the callback function that is called when a request is made to the server. It will return a response to the client.
-const serveHtmlPages = function (req, res) {
+const serveHtmlPages = async function (req, res) {
   // If request is sent from browser - it may ask for a favicon. If so provide one.
   if (req.url.includes(".ico")) {
     serveAdobeFavicon(req, res);
     return;
   }
 
-  const fileRoute = req.url === "/" ? "/index" : req.url;
+  // TODO - allow routing again
+  //   const fileRoute = req.url === "/" ? "/index" : req.url;
   // Set the content header to HTML
-  fs.readFile(__dirname + `/app${fileRoute}.html`)
-    .then((page) => {
-      res.setHeader("Content-Type", "text/html");
-      res.writeHead(200);
-      res.end(page);
-    })
-    .catch((err) => {
-      // TODO - serve 404 page if file not found
-      console.log(err);
-      res.writeHead(500);
-      res.end(err);
-      return;
-    });
+  const parser = new SSIHtmlParser(__dirname + `/app/index.shtml`);
+
+  try {
+    const page = await parser.parse();
+    res.setHeader("Content-Type", "text/html");
+    res.writeHead(200);
+    res.end(page);
+  } catch (err) {
+    // TODO - serve 404 page if file not found
+    console.log(err);
+    res.writeHead(500);
+    res.end(err);
+    return;
+  }
 };
 
 const server = http.createServer(serveHtmlPages);
